8421540f6d7bbd3b5522370ae7265816c290d324
oscar <oscar@orangewood.myguest.virtualbox.org>
2024-11-09 19:19:32 +0530

    first commit

---
+++ build/ros_session/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/oscar/Downloads/ros_ws/src/ros_session
+# Build directory: /home/oscar/Downloads/ros_ws/build/ros_session
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ src/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/oscar/Downloads/ros_ws/src/src
+# Build directory: /home/oscar/Downloads/ros_ws/src/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
